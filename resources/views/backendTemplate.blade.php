5d262e79f044ec1719d47f54bf3cd3a63da2b95f
nyyelin <nyiyelin4@gmail.com>
2022-12-07 17:42:54 +0630

    sweet alert2

--- resources/views/backendTemplate.blade.php
+++ resources/views/backendTemplate.blade.php
@@ -10,12 +10,14 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{asset('backend/img/favicon.png')}}" rel="icon">
+  <link href="{{ asset('backend/img/favicon.png') }}" rel="icon">
   <link href="{{ asset('backend/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="{{ asset('backend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -23,6 +25,9 @@
   <link href="{{ asset('backend/vendor/quill/quill.snow.css') }}" rel="stylesheet">
   <link href="{{ asset('backend/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
   <link href="{{ asset('backend/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+  {{-- sweetalert3 --}}
+  <link href="{{ asset('plugin/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
@@ -82,7 +87,8 @@
     </div>
   </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="{{ asset('backend/vendor/apexcharts/apexcharts.min.js') }}"></script>
@@ -94,9 +100,19 @@
   <script src="{{ asset('backend/vendor/tinymce/tinymce.min.js') }}"></script>
   <script src="{{ asset('backend/vendor/php-email-form/validate.js') }}"></script>
 
+  {{-- sweetalert2 --}}
+  <script src="{{ asset('plugin/sweetalert2/sweetalert2.all.min.js') }}"></script>
   <!-- Template Main JS File -->
   <script src="{{ asset('backend/js/main.js') }}"></script>
-
+  <script>
+    $(document).ready(function() {
+      Swal.fire({
+        title: 'Success',
+        icon: "success",
+        button: "Ok!",
+      });
+    })
+  </script>
 </body>
 
 </html>
