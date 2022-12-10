71bfaed9c9ceca56a5b74395b7bd0ccce9a8b5a9
nyyelin <nyiyelin4@gmail.com>
2022-12-10 23:13:30 +0630

    dataTable

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('backend.dashboard');
 });
+
+
+Route::get('/mainCategory', function () {
+    return view('backend.category.mainCategory');
+});
